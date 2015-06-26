51332965ebd00bc957fc5fcaa2f70a6868c11063
Nick Gartmann <nick.gartmann@gmail.com>
2015-06-25 19:22:57 -0500

    added first cut of templates

---
+++ lib/phoenix/generate.rb
@@ -0,0 +1,9 @@
+require "phoenix/generate/version"
+
+module Phoenix
+  module Generate
+    def self.generate
+      puts "THIS IS THE THING"
+    end
+  end
+end
---
+++ lib/phoenix/generate/version.rb
@@ -0,0 +1,5 @@
+module Phoenix
+  module Generate
+    VERSION = "0.1.0"
+  end
+end
