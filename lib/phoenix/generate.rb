4e96967e9f737f083077e224305cb64a649df56f
Nick Gartmann <nick.gartmann@gmail.com>
2015-07-10 17:54:13 -0500

    meh

--- lib/phoenix/generate.rb
+++ lib/phoenix/generate.rb
@@ -8,6 +8,16 @@ require "fileutils"
 module Phoenix
   module Generate
 
+    class Node
+      attr_accessor :value
+      attr_accessor :parents
+      def initialize(value, parents)
+        @value = value
+        @parents = parents
+      end
+
+    end
+
     class ERBContext
       def initialize(schema)
         (class << self; self; end).class_eval do
@@ -50,7 +60,28 @@ module Phoenix
       FileUtils.mkdir_p(target_directory)
       begin
         json = JSON.parse(STDIN.read)
+
+        json["models"] = json["models"].sort do |a,b|
+          ret = 0
+          ret = -1 if a["relationships"].nil?
+          ret = 1 if b["relationships"].nil?
+          if ret === 0
+            if a["relationships"].map{|rel| rel["type"]}.include?(b["type"]) && ["many_to_one"].include?(a["relationships"].find {|c| c["type"] == b["type"]}["rel"])
+              ret = 1
+            elsif b["relationships"].map{|rel| rel["type"]}.include?(a["type"]) && ["many_to_one"].include?(b["relationships"].find {|c| c["type"] == a["type"]}["rel"])
+              ret = -1
+            else
+              ret = 0
+            end
+          end
+          ret
+        end.each_with_index.map{|x, i|
+          x["index"] = Time.now.strftime("%Y%m%d%H%M%S%L") + i.to_s
+          x
+        }
+
         schema = ERBContext.new(json)
+
         Dir["./templates/api/**/*"].each do |file|
           full = File.join(Dir.pwd, file)
           fname = file[16..-1]
@@ -63,12 +94,15 @@ module Phoenix
           else
             unless file.match(/\.erb/).nil?
               if /\*.+?\$.+?\$/ =~ file
-                matches = file.match(/\*(.+?)\$(.+?)\$/)
+                repeats = file.match(/\*(.+?)\$/)
+                replaces = file.scan(/\$(.+?)\$/)
                 tfname = fname
-                schema.send(matches[1].to_sym).each do |item|
+                schema.send(repeats[1].to_sym).each do |item|
                   fname = tfname[0..-5]
-                  fname = fname.sub!("*#{matches[1]}", "")
-                  fname = fname.sub!("$#{matches[2]}$", item.send(matches[2].to_sym))
+                  fname = fname.sub!("*#{repeats[1]}", "")
+                  replaces.flatten.each do |replace|
+                    fname = fname.sub!("$#{replace}$", item.send(replace.to_sym).to_s)
+                  end
                   f = File.new(File.join(target_directory, fname), "w")
                   f.write ERB.new(File.read(file), nil, "-").result(ModelContext.new(json, item).bind)
                   f.close
